c817bc62e58ca18b4c77c4d3d606d2022741f86c
mayank kumar <56073843+mayank0508@users.noreply.github.com>
2020-12-04 22:54:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -90,40 +90,6 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
     default:
       logEntry = {};
   }
-  // if (ev === LOG_EVENT_PLAYER_ATTACK) {
-  //   logEntry.target = 'MONSTER';
-  // } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-  //   logEntry = {
-  //     event: ev,
-  //     value: val,
-  //     target: 'MONSTER',
-  //     finalMonsterHealth: monsterHealth,
-  //     finalPlayerHealth: playerHealth
-  //   };
-  // } else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-  //   logEntry = {
-  //     event: ev,
-  //     value: val,
-  //     target: 'PLAYER',
-  //     finalMonsterHealth: monsterHealth,
-  //     finalPlayerHealth: playerHealth
-  //   };
-  // } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-  //   logEntry = {
-  //     event: ev,
-  //     value: val,
-  //     target: 'PLAYER',
-  //     finalMonsterHealth: monsterHealth,
-  //     finalPlayerHealth: playerHealth
-  //   };
-  // } else if (ev === LOG_EVENT_GAME_OVER) {
-  //   logEntry = {
-  //     event: ev,
-  //     value: val,
-  //     finalMonsterHealth: monsterHealth,
-  //     finalPlayerHealth: playerHealth
-  //   };
-  // }
   battleLog.push(logEntry);
 }
 
